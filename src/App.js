67b69ef60426ad3c6b7077784448e6b1cf2d9790
Hunan1598 <phunan98@mail.ru>
2020-08-29 21:42:53 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Info from "./Components/info";
+
+
+
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+       <h1>sggds</h1>
+        <info />
+      </div>
+
+    );
+  }
+}
+
+export default App;
